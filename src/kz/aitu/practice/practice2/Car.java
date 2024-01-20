c4e1049281a930dd5adbef0eccf3a37f38cc16a5
Zharych Utegenov <uzharken@gmail.com>
2024-01-20 14:41:23 +0600

    Initial commit

---
+++ src/kz/aitu/practice/practice2/Car.java
@@ -0,0 +1,13 @@
+package kz.aitu.practice.practice2;
+
+public class Car {
+    private int capacity;
+
+    public Car(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
